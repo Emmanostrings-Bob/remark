ba45e48d766d162d347666807f1825ff787b12a9
Emmanostrings-Bob <oluwafemiayomide852@gmail.com>
2024-07-30 13:07:05 +0100

    another update

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className="w-full h-full flex-grow pt-10">
       <div className="w-full max-h-[712px] md:max-h-[792px] h-[100svh] relative flex z-[1]">
-      <Image src='/assets/home-image.jpeg' alt='hero image background' layout='fill' style={{ objectFit: 'cover' }} />
+      <Image src='/assets/home-image.jpeg' alt='hero image background' fill style={{ objectFit: 'cover' }} />
         <div className="h-full w-full absolute bg-gradient-to-t pb-[72px] md:pb-[168px] px-4 md:px-8 lg:px-14 from-black via-black/70 to-black/10 flex flex-col justify-end items-start">
           <div className='flex flex-col justify-start items-start gap-y-10 w-full'>
             <h1 className="text-white md:text-7xl font-bold md:leading-[88px] tracking-tighter sm:tracking-normal xs:text-[40px] text-[38px] leading-[56px]">
